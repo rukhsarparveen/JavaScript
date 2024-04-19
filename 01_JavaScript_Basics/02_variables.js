dad342313259765bf89ed56dd52bda44d388fcf6
rukhsarparveen <parveenrukhsar0605@gmail.com>
2024-04-19 17:46:15 +0530

    data-type started

---
+++ 01_JavaScript_Basics/02_variables.js
@@ -0,0 +1,14 @@
+const accountNum = 2
+let accountName = "Jannat"
+var accountState = "delhi"
+accountCountry = "India"
+
+console.log(accountNum);
+console.log([accountName, accountState, accountCountry]);
+
+
+/*
+const = use when you don't want to change the value of variable, it is important to initialize the variable while declaring it.
+let and var = you can use their values, There is the scope problem in var which is solved by let.
+if we dont initialize let or var it gives undefined value.
+*/
---
+++ 01_JavaScript_Basics/03_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hi")  // this will give error because we are using nodejs not browser
+
+//number
+//bigInt
+//string
+//boolean
+//null => standalone value /it means it is empty
+// undefined => means value not defined
+//symbol => unique
+// objects
+
+// type of null is object but type of undefined is undefined
+
+let nullCheck = null
+let undefinedCheck
+
+console.log(typeof nullCheck)  // object
+console.log(typeof undefinedCheck) // undefined
